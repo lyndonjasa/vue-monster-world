acbae3afc13025eefd949ca26585901c206dfa0a
Don Jasa <janlyndonjasa@gmail.com>
2021-12-06 16:20:26 +0800

    added burn percentage and wet amplification onto env variables

--- src/hooks/useEnvironment.ts
+++ src/hooks/useEnvironment.ts
@@ -1,10 +1,14 @@
 const useEnvironment = () => {
   const damageMargin: number = process.env.VUE_APP_DAMAGE_MARGIN
   const spriteScale: number = process.env.VUE_APP_SPRITE_SCALE
+  const wetAmplifier: number = process.env.VUE_APP_WET_STAT_AMPLICATION
+  const burnPercentage: number = process.env.VUE_APP_BURN_DMG_PERCENTAGE
 
   return {
     damageMargin,
-    spriteScale
+    spriteScale,
+    wetAmplifier,
+    burnPercentage
   }
 }
 
