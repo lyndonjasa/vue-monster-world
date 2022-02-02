312d55779cdffcf04658175afa57d351a1a06fb9
Don Jasa <janlyndonjasa@gmail.com>
2022-02-02 17:36:58 +0800

    started login component

---
+++ src/components/user/Login.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+const Login = defineComponent({
+  setup() {
+    const username = ref<string>(undefined);
+    const password = ref<string>(undefined);
+    const firstName = ref<string>(undefined);
+    const lastName = ref<string>(undefined);
+
+    return {
+      username,
+      password,
+      firstName,
+      lastName
+    }
+  },
+})
+
+export default Login
+</script>
