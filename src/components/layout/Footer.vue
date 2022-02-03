6695f007aeac13714aa0dfae94865c0b0a454f57
Don Jasa <janlyndonjasa@gmail.com>
2022-02-03 20:29:59 +0800

    added disclaimer

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="footer-wrapper"></div>
+  <div class="footer-wrapper">
+    <p>DISCLAIMER: This is a personal project made by the developer to learn more about the <b>MEVN</b> stack and in no way 
+      does it stand to gain any profit. Most, if not all, images/sprites are captured from the web and are requested 
+      from their creators.
+    </p>
+  </div>
 </template>
 
 <script lang="ts">
@@ -16,5 +21,11 @@ export default Footer;
   height: 40px;
   padding: 5px 10px;
   background-color: black;
+  text-align: center;
+
+  p {
+    color: white;
+    font-size: 12px;
+  }
 }
 </style>
