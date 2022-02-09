ba1d8b967d9808a8dbef59bee05639143ed16920
Don Jasa <janlyndonjasa@gmail.com>
2022-02-09 20:27:46 +0800

    added furhter styles

--- src/components/user/UserAccount.vue
+++ src/components/user/UserAccount.vue
@@ -2,6 +2,13 @@
   <div class="user-account-wrapper">
     <div class="user-account-details app-transluscent-div">
       <p class="user-account-name">{{ account.accountName }}</p>
+      <div class="user-account-monster" v-for="monster in account.monsters" :key="monster.name">
+        <div class="monster-thumbnail"></div>
+        <div class="monster-details">
+          <p>{{ monster.name }}</p>
+          <p>Lvl. {{ monster.level }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -19,7 +26,7 @@ const UserAccount = defineComponent({
 export default UserAccount;
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .user-account-details {
   width: 210px;
   padding: 20px;
@@ -38,6 +45,22 @@ export default UserAccount;
     font-size: 24px;
     padding-bottom: 15px;
     border-bottom: 2px solid white;
+  }
+
+  .user-account-monster {
+    margin-top: 25px;
+    display: flex;
+
+    .monster-thumbnail {
+      height: 50px;
+      width: 50px;
+      background-color: yellow;
+    }
+
+    .monster-details {
+      margin-left: 10px;
+      font-size: 14px;
+    }
   }
 }
 </style>
